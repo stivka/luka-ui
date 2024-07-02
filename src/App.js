44c5dcd8296af804bdf92ae51111a68376c630e2
Markus Tarn <markustarn@gmail.com>
2024-07-02 20:06:40 +0300

    Added react-query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
-import {ThemeProvider, CssBaseline} from '@mui/material';
-import Login from './components/Login';
-import MainApp from './components/MainApp';
-import theme from './theme'; // Ensure you have a theme file
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import {
+	Navigate,
+	Route,
+	BrowserRouter as Router,
+	Routes,
+} from "react-router-dom";
+import Login from "./components/Login";
+import MainApp from "./components/MainApp";
+import theme from "./theme"; // Ensure you have a theme file
+
+const queryClient = new QueryClient();
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Login/>}/>
-                    <Route path="/app" element={<MainApp/>}/>
-                    <Route path="*" element={<Navigate to="/"/>}/>
-                </Routes>
-            </Router>
-        </ThemeProvider>
-    );
+	return (
+		<ThemeProvider theme={theme}>
+			<QueryClientProvider client={queryClient}>
+				<CssBaseline />
+				<Router>
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/app" element={<MainApp />} />
+						<Route path="*" element={<Navigate to="/" />} />
+					</Routes>
+				</Router>
+			</QueryClientProvider>
+		</ThemeProvider>
+	);
 }
 
 export default App;
