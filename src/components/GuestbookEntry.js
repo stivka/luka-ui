4a3f29b10f6b0c75f79bc1564c315b49f6e240fb
Markus Tarn <markustarn@gmail.com>
2024-07-23 13:44:24 +0300

    Fixed guestbook entry using date instead of createdAt timestamp

--- src/components/GuestbookEntry.js
+++ src/components/GuestbookEntry.js
@@ -1,6 +1,5 @@
 import { Card, CardContent, Typography } from "@mui/material";
 import React from "react";
-// const { format, formatDistanceToNow } = require("date-fns");
 const { formatDistanceToNow } = require("date-fns");
 
 const GuestbookEntry = ({ entry }) => (
@@ -15,12 +14,8 @@ const GuestbookEntry = ({ entry }) => (
 			<Typography variant="h6" align="center">
 				{entry.name}
 			</Typography>
-			{/* Use this one for the real datetime in previous format */}
-			{/* <Typography variant="body2" color="white" align="center">
-				{format(entry.date, "HH:mm:ss dd/MM/yy")}
-			</Typography> */}
 			<Typography variant="body2" color="white" align="center">
-				{formatDistanceToNow(new Date(entry.date), { addSuffix: true })}
+				{formatDistanceToNow(new Date(entry.createdAt), { addSuffix: true })}
 			</Typography>
 			<Typography variant="body1" align="center">
 				{entry.message}
